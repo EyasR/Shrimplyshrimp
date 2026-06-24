705318af297f3669381ea3e03ff30edb00c66bcf
aglanfield-design <aglanfield@gulfstreamschool.org>
2026-06-24 11:58:48 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-let numButtonClicks = 0;
-function buttonClicked() {
-    numButtonClicks = numButtonClicks + 1;
-    document.getElementById("mainDiv").textContent =
-        "Button Clicked times: " + numButtonClicks;
-}
+const faqs= document.querySelectorAll(".faq-question");
+console.log("program start");
+faqs.forEach(faq =>{
+    faq.addEventListener("click",function(){
+        console.log("clicked");
+        this.classList.toggle("active");
+        const answer = this.nextElementSibling;
+        if(answer.style.maxHeight){
+            answer.style.maxHeight = null;
+        }else{
+            answer.style.maxHeight = answer.scrollHeight + "px";
+        }
+    });
+    
+});
